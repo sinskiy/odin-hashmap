60ec5f89d2574de17ebf84b6f681f9ff001ff440
sinskiy <sinskiy@proton.me>
2024-07-03 13:00:38 +0300

    feat: add hash method

--- main.js
+++ main.js
@@ -2,4 +2,15 @@
 //   throw new Error("Trying to access index out of bound");
 // }
 
-class HashMap {}
+class HashMap {
+  hash(key) {
+    let hashCode = 0;
+
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = primeNumber * hashCode + key.charCodeAt(i);
+    }
+
+    return hashCode;
+  }
+}
